934bca8f6550fa777c1c09655e8a5cdcf5be15f1
EAJH <90656408+EAJH@users.noreply.github.com>
2022-09-21 10:14:22 -0500

    Update Piloto.java

--- Piloto.java
+++ Piloto.java
@@ -1,8 +1,10 @@
+import java.util.Random;
 public class Piloto{
-    String nombre;
-    String clave;
-    String nacionalidad;
-    int edad;
+    //Atributos
+    private String nombre;
+    private String clave;
+    private String nacionalidad;
+    private int edad;
 
     //Constructores
     public Piloto(){
@@ -15,5 +17,22 @@ public class Piloto{
         this.nacionalidad=nacionalidad;
         this.edad=edad;
     }
-    
+    //Setters y getters
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+
+    public String getNombre(){
+        return this.nombre;
+    }
+
+    public void setClave(){
+        this.clave=//poner método generador de clave;
+    }
+
+    public String generarCLave(){
+        String alf = "abcdefghijklmnopqrstuvwxyz";
+        //Buscar función en random que tome valores al azar de un String
+        //También podría ser generar arreglo de caracteres, tener un índice aleatorio y añadirlos al StringBuildet con appendX 
+    }
 }
