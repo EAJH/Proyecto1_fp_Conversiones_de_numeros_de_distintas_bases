f4e3e1b125bd4ca0ecda8bf9615e9524baa3afd2
EAJH <90656408+EAJH@users.noreply.github.com>
2022-09-21 10:01:35 -0500

    Create Piloto.java

---
+++ Piloto.java
@@ -0,0 +1,19 @@
+public class Piloto{
+    String nombre;
+    String clave;
+    String nacionalidad;
+    int edad;
+
+    //Constructores
+    public Piloto(){
+    
+    }
+
+    public Piloto(String nombre, String clave, String nacionalidad, int edad){
+        this.nombre=nombre;
+        this.clave=clave;
+        this.nacionalidad=nacionalidad;
+        this.edad=edad;
+    }
+    
+}
